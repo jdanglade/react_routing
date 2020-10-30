07c9d195dac03a5a864d30946e8e4bc5fb363eba
Justin Danglade <just.danglade@gmail.com>
2020-10-30 08:41:31 -0400

    playing with link styles

--- src/Nav.js
+++ src/Nav.js
@@ -1,20 +1,17 @@
 import React from "react";
 import "./App.css";
-import { Link, withRouter } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Nav() {
-  const navStyle = {
-    color: "white",
-  };
   return (
     <div className="Nav">
       <nav>
         <h3>Logo</h3>
         <ul className="nav-links">
-          <Link style={navStyle} to="/about">
+          <Link className="link" to="/about">
             <li>About</li>
           </Link>
-          <Link style={navStyle} to="/shop">
+          <Link className="link" to="/shop">
             <li>Shop</li>
           </Link>
         </ul>
